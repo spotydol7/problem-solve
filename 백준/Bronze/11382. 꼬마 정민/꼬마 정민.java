7e1e52fead740e83dd25419ac653a1e12c3102a8
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-01-22 21:10:24 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static long a, b, c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
+        c = Long.parseLong(st.nextToken());
+
+        System.out.println(a + b + c);
+    }
+}
