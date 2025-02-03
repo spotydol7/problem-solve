6852349f34ff969e87a6f96b91abf26c94e9dfe2
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-02-03 21:10:11 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int[][] board = new int[101][101];
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for (int j = b; j < b + 10; j++) {
+                for (int k = a; k < a + 10; k++) {
+                    board[j][k] = 1;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                cnt += board[i][j];
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
