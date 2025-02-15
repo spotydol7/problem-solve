64093a63a08ba40a613449a8d9ac879bbd1422b2
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-02-16 02:42:37 +0900

    [Silver III] Title: N과 M (4), Time: 244 ms, Memory: 19284 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    public static void dfs(int level, int num) {
+        StringBuilder sb = new StringBuilder();
+        if (level == m) {
+            for (int val : arr) {
+                sb.append(val + " ");
+            }
+            System.out.println(sb);
+            return;
+        }
+        for (int i = num; i <= n; i++) {
+                arr[level] = i;
+                dfs(level + 1, i);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        dfs(0, 1);
+    }
+}
