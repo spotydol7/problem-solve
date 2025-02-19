8b77c3e54277a2ca9bff20a79afb5e1e21d1f6d2
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-02-19 12:32:53 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 34 ms, Memory: 13,624 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.cpp
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.cpp
@@ -1,60 +1,62 @@
-#include<iostream>
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-using namespace std;
-
-int plane[101][101];
-int visited[101][101];
-
-int dx[] = {1, -1, 0};
-int dy[] = {0, 0, -1};
-int ans;
-
-bool check(int x, int y) {
-	if (x < 0 || y < 0 || x >= 100 || y >= 100) return false;
-    return true;
-}
-
-void dfs(int x, int y) {
-    if (y == 0) {
-        ans = x;
-    	return;
-    }
-    for (int i = 0; i < 3; i++) {
-    	int nx = x + dx[i];
-        int ny = y + dy[i];
-        if (check(nx, ny) && plane[ny][nx] != 0 && !visited[ny][nx]) {
-        	visited[ny][nx] = 1;
-            dfs(nx, ny);
-            return;
-        }
-    }
-}
-
-int main(int argc, char** argv)
-{
-	int test_case;
-	int T = 10;
-
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-        int x, y;
-        int t;
-        cin >> t;
-        for (int i = 0; i < 100; i++) {
-        	for (int j = 0; j < 100; j++) {
-            	cin >> plane[i][j];
-                visited[i][j] = 0;
-                if (plane[i][j] == 2) {
-                	x = j;
-                    y = i;
-                }
-            }
-        }
-        
-        visited[y][x] = 1;
-        dfs(x, y);
-        cout << "#" << test_case << ' ' << ans << endl;
-        
+public class Solution {
+	static int[][] board;
+	static boolean[][] visited;
+	
+	static int[] dx = {1, -1, 0};
+	static int[] dy = {0, 0, -1};
+	static int ans;
+	
+	public static boolean check(int x, int y) {
+		if (x < 0 || y < 0 || x >= 100 || y >= 100) return false;
+	    return true;
 	}
-	return 0;
+	
+	public static void dfs(int x, int y) {
+		if (y == 0) {
+	        ans = x;
+	    	return;
+	    }
+	    for (int i = 0; i < 3; i++) {
+	    	int nx = x + dx[i];
+	        int ny = y + dy[i];
+	        if (check(nx, ny) && board[ny][nx] != 0 && !visited[ny][nx]) {
+	        	visited[ny][nx] = true;
+	            dfs(nx, ny);
+	            return;
+	        }
+	    }
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			int x = 0, y = 0;
+			int t = Integer.parseInt(br.readLine());
+			
+			board = new int[100][100];
+			visited = new boolean[100][100];
+			
+			for (int i = 0; i < 100; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < 100; j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+					visited[i][j] = false;
+					if (board[i][j] == 2) {
+						x = j;
+						y = i;
+					}
+				}
+			}
+			
+			visited[y][x] = true;
+	        dfs(x, y);
+	        
+	        System.out.println("#" + test_case + " " + ans);
+		}
+	}
 }
