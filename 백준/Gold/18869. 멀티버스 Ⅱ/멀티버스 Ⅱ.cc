5a0c3087b7506979374a09bb6f5939b60600656c
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-03-20 17:49:04 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 532 ms, Memory: 6172 KB -BaekjoonHub

---
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int ans;
+vector<int> arr;
+vector<vector<int>> space;
+
+void coordinateCompress(vector<int> v, vector<int> v2) {
+	vector<int> temp_v;
+	for (int i = 0; i < m; i++) {
+		int idx = lower_bound(v.begin(), v.end(), v2[i]) - v.begin();
+		temp_v.push_back(idx);
+	}
+	space.push_back(temp_v);
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		vector<int> v;
+		vector<int> v2;
+		for (int j = 0; j < m; j++) {
+			int temp;
+			cin >> temp;
+			v.push_back(temp);
+			v2.push_back(temp);
+		}
+		sort(v.begin(), v.end());
+		v.erase(unique(v.begin(), v.end()), v.end());
+		
+		coordinateCompress(v, v2);
+	}
+
+	for (int i = 0; i < n; i++) {
+		
+		for (int j = i + 1; j < n; j++) {
+			bool flag = true;
+			for (int k = 0; k < m; k++) {
+				if (space[i][k] != space[j][k]) flag = false;
+			}
+			if (flag) ans++;
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
