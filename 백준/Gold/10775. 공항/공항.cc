b7750f91cc525258ad4e993a60532efe05f3b9d0
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-04-07 09:48:46 +0900

    [Gold II] Title: 공항, Time: 48 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int parent[100001];
+
+int findParent(int x) {
+	if (x == parent[x]) return x;
+	return parent[x] = findParent(parent[x]);
+}
+
+void uniParent(int a, int b) {
+	a = findParent(a);
+	b = findParent(b);
+	if (a == b) return;
+	parent[a] = b;
+}
+
+int main() {
+	int g, p;
+	cin >> g >> p;
+	
+	// i번째 비행기를 1번부터 gi (1 ≤ gi ≤ G) 번째 게이트중 하나에 영구적으로 도킹
+	vector<int> gis;
+	for (int i = 0; i < p; i++) {
+		int temp;
+		cin >> temp;
+		gis.push_back(temp);
+	}
+
+	for (int i = 1; i <= g; i++) {
+		parent[i] = i;
+	}
+
+	int ans = 0;
+	for (int i = 0; i < gis.size(); i++) {
+		if (findParent(gis[i]) == 0) break;
+		else {
+			parent[findParent(gis[i])] = findParent(findParent(gis[i]) - 1);
+			ans++;
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
