609d84ce162ca55cedeb5bb558c289b933e154f7
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-04-08 11:25:28 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long dp[5001];
+
+int main() {
+	string str;
+	cin >> str;
+	
+	long long ans = 0;
+	
+	if (str[0] == '0') {
+		cout << ans;
+		return 0;
+	}
+
+	dp[0] = 1;
+	dp[1] = 1;
+	
+	for (int i = 2; i <= str.length(); i++) {
+		if (str[i - 1] != '0') dp[i] = dp[i - 1] % 1000000;
+
+		int alpha = (str[i - 1] - '0') + 10 * (str[i - 2] - '0');
+		if (alpha >= 10 && alpha <= 26) {
+			dp[i] = (dp[i] + dp[i - 2]) % 1000000;
+		}
+	}
+
+	cout << dp[str.length()];
+
+	return 0;
+}
