c325d91828f16d063d9d9e8f955a6f0a1c6f4fbf
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-08-05 22:03:27 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 424 ms, Memory: 45088 KB -BaekjoonHub

---
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] cost = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            cost[i] = b - a;
+        }
+
+        Arrays.sort(cost);
+
+        if (cost[K - 1] < 0) System.out.println(0);
+        else System.out.println(cost[K - 1]);
+    }
+}
