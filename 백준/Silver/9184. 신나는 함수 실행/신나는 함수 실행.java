c346df9162803afd2173dc164d2f3a9ce774b1f2
Jeon Wongyun <61699659+spotydol7@users.noreply.github.com>
2025-08-09 18:01:45 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 512 ms, Memory: 28708 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][][] dp = new int[21][21][21];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) { 
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) return;
+
+            System.out.println("w(" + a + ", " + b + ", " + c + ") = " + dp(a, b, c));
+        }        
+    }
+
+    public static int dp(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) return 1;
+        if (a > 20 || b > 20 || c > 20) return dp(20, 20, 20);
+        if (dp[a][b][c] > 0) return dp[a][b][c];
+        if (a < b && b < c) {
+            dp[a][b][c] = dp(a, b, c - 1) + dp(a, b - 1, c - 1) - dp(a, b - 1, c);
+        }
+        dp[a][b][c] = dp(a - 1, b, c) + dp(a - 1, b - 1, c) + dp(a - 1, b, c - 1) - dp(a - 1, b - 1, c - 1);
+
+        return dp[a][b][c];
+    }
+}
