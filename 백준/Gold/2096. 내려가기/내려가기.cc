399439c8b90ba5141b83b64d1e90aabf314e51f7
Jeon Wongyun <spotydol7@gmail.com>
2025-11-24 23:43:29 +0900

    [Gold V] Title: 내려가기, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -1,27 +1,42 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-long long N, dp_mx[4][2], dp_mn[4][2];
+
+int maxDp[3], minDp[3];
+
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-	cin>>N;
-	for(int i=1; i<=N; i++) {
-        int num1, num2, num3;
-		cin>>num1>>num2>>num3;
-		dp_mx[1][1]=num1+max(dp_mx[1][0], dp_mx[2][0]);
-		dp_mn[1][1]=num1+min(dp_mn[1][0], dp_mn[2][0]);
+    int N;
+    cin >> N;
 
-		dp_mx[2][1]=num2+max({dp_mx[1][0], dp_mx[2][0], dp_mx[3][0]});
-		dp_mn[2][1]=num2+min({dp_mn[1][0], dp_mn[2][0], dp_mn[3][0]});
+    cin >> maxDp[0] >> maxDp[1] >> maxDp[2];
+    minDp[0] = maxDp[0];
+    minDp[1] = maxDp[1];
+    minDp[2] = maxDp[2];
 
-		dp_mx[3][1]=num3+max(dp_mx[3][0], dp_mx[2][0]);
-		dp_mn[3][1]=num3+min(dp_mn[3][0], dp_mn[2][0]);
+    for (int i = 1; i < N; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
 
-		for(int i=1; i<=3; i++) {
-			dp_mx[i][0]=dp_mx[i][1];
-			dp_mn[i][0]=dp_mn[i][1];
-		}
-	}
-	cout<<max({dp_mx[1][0], dp_mx[2][0], dp_mx[3][0]})<<" "<<min({dp_mn[1][0], dp_mn[2][0], dp_mn[3][0]});
+        int nextMax0 = max(maxDp[0], maxDp[1]) + a;
+        int nextMax1 = max(max(maxDp[0], maxDp[1]), maxDp[2]) + b;
+        int nextMax2 = max(maxDp[1], maxDp[2]) + c;
 
+        maxDp[0] = nextMax0;
+        maxDp[1] = nextMax1;
+        maxDp[2] = nextMax2;
+
+        int nextMin0 = min(minDp[0], minDp[1]) + a;
+        int nextMin1 = min(min(minDp[0], minDp[1]), minDp[2]) + b;
+        int nextMin2 = min(minDp[1], minDp[2]) + c;
+
+        minDp[0] = nextMin0;
+        minDp[1] = nextMin1;
+        minDp[2] = nextMin2;
+    }
+
+    int maximum = max(max(maxDp[0], maxDp[1]), maxDp[2]);
+    int minimum = min(min(minDp[0], minDp[1]), minDp[2]);
+
+    cout << maximum << ' ' << minimum << '\n';
+
+    return 0;
 }
