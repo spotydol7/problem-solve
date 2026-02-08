843babdf408f219cf254de3e1e3cd5e870a62926
Jeon Wongyun <spotydol7@gmail.com>
2026-02-08 20:11:09 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 10940 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,79 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int r, c;
-string maze[1001];
+int R, C;
+int dr[4] = { 0, 0, 1, -1 };
+int dc[4] = { 1, -1, 0, 0 };
 int run[1001][1001];
 int fire[1001][1001];
-int dx[] = { 1, 0, -1, 0 };
-int dy[] = { 0, 1, 0, -1 };
+string board[1001];
 queue<pair<int, int>> run_q;
 queue<pair<int, int>> fire_q;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> r >> c;
-	memset(run, -1, sizeof(run));
-	memset(fire, -1, sizeof(fire));
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	for (int i = 0; i < r; i++) {
-		cin >> maze[i];
-	}
+    memset(fire, -1, sizeof(fire));
+    memset(run, -1, sizeof(run));
 
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			if (maze[i][j] == 'J') {
-				run_q.push({ j, i });
-				run[i][j] = 0;
-			}
-			if (maze[i][j] == 'F') {
-				fire_q.push({ j, i });
-				fire[i][j] = 0;
-			}
-		}
-	}
+    cin >> R >> C;
+    for (int i = 0; i < R; i++) {
+        cin >> board[i];
+        for (int j = 0; j < C; j++) {
+            if (board[i][j] == 'J') {
+                run_q.push({ i, j });
+                run[i][j] = 0;
+            }
+            if (board[i][j] == 'F') {
+                fire_q.push({ i, j });
+                fire[i][j] = 0;
+            }
+        }
+    }
 
-	while (!fire_q.empty()) {
-		auto cur = fire_q.front();
-		fire_q.pop();
-		int x = cur.first;
-		int y = cur.second;
+    while (!fire_q.empty()) {
+        auto cur = fire_q.front();
+        fire_q.pop();
+        int cur_r = cur.first;
+        int cur_c = cur.second;
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+        for (int i = 0; i < 4; i++) {
+            int nr = cur_r + dr[i];
+            int nc = cur_c + dc[i];
 
-			if (nx < 0 || nx >= c || ny < 0 || ny >= r) continue;
-			if (fire[ny][nx] >= 0 || maze[ny][nx] == '#') continue;
+            if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+            if (board[nr][nc] == '#' || fire[nr][nc] >= 0) continue;
+            fire[nr][nc] = fire[cur_r][cur_c] + 1;
+            fire_q.push({ nr, nc });
+        }
+    }
 
-			fire_q.push({ nx, ny });
-			fire[ny][nx] = fire[y][x] + 1;
-		}
-	}
+    while (!run_q.empty()) {
+        auto cur = run_q.front();
+        run_q.pop();
+        int cur_r = cur.first;
+        int cur_c = cur.second;
 
-	while (!run_q.empty()) {
-		auto cur = run_q.front();
-		run_q.pop();
-		int x = cur.first;
-		int y = cur.second;
+        for (int i = 0; i < 4; i++) {
+            int nr = cur_r + dr[i];
+            int nc = cur_c + dc[i];
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+            if (nr < 0 || nr >= R || nc < 0 || nc >= C) {
+                cout << run[cur_r][cur_c] + 1 << '\n';
+                return 0;
+            }
 
-			if (nx < 0 || nx >= c || ny < 0 || ny >= r) { // maze 가장자리 도착 == 탈출
-				cout << run[y][x] + 1;
-				return 0;
-			}
-			if (run[ny][nx] >= 0 || maze[ny][nx] == '#') continue;
-			if (fire[ny][nx] != -1 && fire[ny][nx] <= run[y][x] + 1) continue;
+            if (run[nr][nc] >= 0 || board[nr][nc] == '#') continue;
+            if (fire[nr][nc] != -1 && fire[nr][nc] <= run[cur_r][cur_c] + 1) continue;
 
-			run[ny][nx] = run[y][x] + 1;
-			run_q.push({ nx, ny });
-		}
-	}
-	cout << "IMPOSSIBLE";
+            run[nr][nc] = run[cur_r][cur_c] + 1;
+            run_q.push({ nr, nc });
+        }
+    }
 
-	return 0;
+    cout << "IMPOSSIBLE" << '\n';
+
+    return 0;
 }
