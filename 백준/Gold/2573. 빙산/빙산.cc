91dd79510d49db8fd20c7f0d3816459e772662e4
Jeon Wongyun <spotydol7@gmail.com>
2026-02-11 05:53:41 +0900

    [Gold IV] Title: 빙산, Time: 96 ms, Memory: 2820 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -1,97 +1,102 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
-int arr[301][301];
-int visited[301][301];
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+int R, C;
+int board[301][301];
+int tmp_board[301][301];
+bool visited[301][301];
+int dr[4] = { 1, -1, 0, 0 };
+int dc[4] = { 0, 0, 1, -1 };
 
-void melt() {
-    int temp[301][301] = {0};
+bool check() {
+	memset(visited, false, sizeof(visited));
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (arr[i][j] > 0) {
-                int water = 0;
-                for (int d = 0; d < 4; d++) {
-                    int nx = i + dx[d];
-                    int ny = j + dy[d];
-                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] == 0) {
-                        water++;
-                    }
-                }
-                temp[i][j] = max(0, arr[i][j] - water);
-            }
-        }
-    }
+	int cnt = 0;
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			if (!visited[i][j] && board[i][j] > 0) {
+				queue<pair<int, int>> q;
+				q.push({ i, j });
+				visited[i][j] = true;
 
-    memcpy(arr, temp, sizeof(arr));
+				while (!q.empty()) {
+					int curR = q.front().first;
+					int curC = q.front().second;
+					q.pop();
+
+					for (int dir = 0; dir < 4; dir++) {
+						int nr = curR + dr[dir];
+						int nc = curC + dc[dir];
+
+						if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+						if (visited[nr][nc] || board[nr][nc] == 0) continue;
+						visited[nr][nc] = true;
+						q.push({ nr, nc });
+					}
+				}
+				cnt++;
+			}
+		}
+	}
+
+	if (cnt >= 2) return true;
+	return false;
 }
 
-void bfs(int x, int y) {
-    queue<pair<int, int>> q;
-    q.push({x, y});
-    visited[x][y] = 1;
+int count_zeros(int r, int c) {
+	int ret = 0;
+	for (int i = 0; i < 4; i++) {
+		int nr = r + dr[i];
+		int nc = c + dc[i];
 
-    while (!q.empty()) {
-        int curx = q.front().first;
-        int cury = q.front().second;
-        q.pop();
-
-        for (int d = 0; d < 4; d++) {
-            int nx = curx + dx[d];
-            int ny = cury + dy[d];
-            if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                if (!visited[nx][ny] && arr[nx][ny] > 0) {
-                    visited[nx][ny] = 1;
-                    q.push({nx, ny});
-                }
-            }
-        }
-    }
-}
-
-int cnt() {
-    memset(visited, 0, sizeof(visited));
-    int count = 0;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (!visited[i][j] && arr[i][j] > 0) {
-                bfs(i, j);
-                count++;
-            }
-        }
-    }
-
-    return count;
+		if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+		if (board[nr][nc] == 0) ret++;
+	}
+	return ret;
 }
 
 int main() {
-    cin >> N >> M;
+	cin >> R >> C;
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> board[i][j];
+		}
+	}
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> arr[i][j];
-        }
-    }
+	int year = 0;
+	while (true) {
+		int cnt = 0;
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				cnt += board[i][j];
+			}
+		}
 
-    int year = 0;
+		if (cnt == 0) {
+			cout << 0 << '\n';
+			return 0;
+		}
 
-    while (1) {
-        int num = cnt();
-        if (num == 0) {
-            cout << 0 << '\n';
-            break;
-        }
-        if (num >= 2) {
-            cout << year << '\n';
-            break;
-        }
-        melt();
-        year++;
-    }
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (board[i][j] > 0) {
+					tmp_board[i][j] = board[i][j] - count_zeros(i, j) >= 0 ? board[i][j] - count_zeros(i, j) : 0;
+				}
+			}
+		}
 
-    return 0;
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				board[i][j] = tmp_board[i][j];
+			}
+		}
+		year++;
+
+		if (check()) {
+			cout << year << '\n';
+			return 0;
+		}
+	}
+
+	return 0;
 }
