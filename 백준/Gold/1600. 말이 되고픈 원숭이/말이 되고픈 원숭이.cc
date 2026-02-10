2e447959fe06e7ec3e10a6219f7116606c18d632
Jeon Wongyun <spotydol7@gmail.com>
2026-02-11 04:25:03 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 68 ms, Memory: 3400 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
@@ -1,61 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int K, C, R;
 int board[201][201];
-int visited[201][201][31];
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
-int jumpx[8] = {1, 2, 1, 2, -1, -2, -1, -2};
-int jumpy[8] = {2, 1, -2, -1, 2, 1, -2, -1};
-int k, w, h;
-queue<pair<pair<int, int>, pair<int, int>>> q;
-
-int bfs(int x, int y) {
-    q.push({{x, y}, {0, 0}});
-    visited[x][y][0] = 1;
-
-    while(!q.empty()) {
-        auto cur = q.front();
-        int jumped = cur.second.first;
-        int cnt = cur.second.second;
-        q.pop();
-
-        if (cur.first.first == h - 1 && cur.first.second == w - 1) return cnt;
-        if (jumped < k) {
-            for (int i = 0; i < 8; i++) {
-                int nx = cur.first.first + jumpx[i];
-                int ny = cur.first.second + jumpy[i];
-                if (nx >= h || nx < 0 || ny >= w || ny < 0) continue;
-                if (visited[nx][ny][jumped + 1] != 0 || board[nx][ny] == 1) continue;
-                visited[nx][ny][jumped + 1] = 1;
-                q.push({{nx, ny}, {jumped + 1, cnt + 1}});
-            }
-        }
-        for (int i = 0; i < 4; i++) {
-            int nx = cur.first.first + dx[i];
-            int ny = cur.first.second + dy[i];
-            if (nx >= h || nx < 0 || ny >= w || ny < 0) continue;
-            if (visited[nx][ny][jumped] != 0 || board[nx][ny] == 1) continue;
-            visited[nx][ny][jumped] = 1;
-            q.push({{nx, ny}, {jumped, cnt + 1}});
-        }
-    }
-
-    return -1;
-}
+bool visited[201][201][31]; // R, C, K
+int dr[12] = { -2, -1, 1, 2, 2, 1, -1, -2, 1, -1, 0, 0 };
+int dc[12] = { 1, 2, 2, 1, -1, -2, -2, -1, 0, 0, 1, -1 };
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> k;
-    cin >> w >> h;
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            cin >> board[i][j];
-        }
-    }
+	cin >> K >> C >> R;
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> board[i][j];
+		}
+	}
 
-    cout << bfs(0, 0);
+	queue<tuple<int, int, int, int>> q; // R, C, step, K
+	q.push({ 0, 0, 0, 0 });
+	visited[0][0][0] = true;
+	while (!q.empty()) {
+		auto cur = q.front(); q.pop();
+		int curR = get<0>(cur);
+		int curC = get<1>(cur);
+		int curStep = get<2>(cur);
+		int curK = get<3>(cur);
 
-    return 0;
+		if (curR == R - 1 && curC == C - 1) {
+			cout << curStep << '\n';
+			return 0;
+		}
+
+		for (int i = 0; i < 12; i++) {
+			if (i >= 0 && i <= 7 && curK >= K) continue;
+			int nr = curR + dr[i];
+			int nc = curC + dc[i];
+			int nK = i >= 0 && i <= 7 ? curK + 1 : curK;
+
+			if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+			if (visited[nr][nc][nK] || board[nr][nc] != 0) continue;
+
+			visited[nr][nc][nK] = true;
+			q.push({ nr, nc, curStep + 1, nK });
+		}
+	}
+
+	cout << -1 << '\n';
+
+	return 0;
 }
