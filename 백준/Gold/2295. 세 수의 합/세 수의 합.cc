eb150125748c23560a42a0e0ee23559e48644e2a
Jeon Wongyun <spotydol7@gmail.com>
2026-02-12 10:56:17 +0900

    [Gold IV] Title: 세 수의 합, Time: 72 ms, Memory: 8188 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -1,59 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<int> arr;
-vector<int> sums;
-
-int binarySearch(int num, int start, int end) {
-	int mid;
-
-	while (start <= end) {
-		mid = (start + end) / 2;
-		if (num == sums[mid])
-			return mid;
-		else if (num < sums[mid])
-			end = mid - 1;
-		else if (num > sums[mid])
-			start = mid + 1;
-	}
-	return -1;
-}
-
 int main() {
-	cin >> n;
+	int N;
+	cin >> N;
 
-	for (int i = 0; i < n; i++) {
+	vector<int> v;
+	for (int i = 0; i < N; i++) {
 		int temp;
 		cin >> temp;
-		arr.push_back(temp);
+		v.push_back(temp);
 	}
 
-	sort(arr.begin(), arr.end());
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <= i; j++) {
-			sums.push_back(arr[i] + arr[j]);
+	vector<int> sum;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			sum.push_back(v[i] + v[j]);
 		}
 	}
 
-	sort(sums.begin(), sums.end());
+	sort(v.begin(), v.end());
+	sort(sum.begin(), sum.end());
 
-	/*
-	cout << "sums : " << endl;
-	for (int i = 0; i < sums.size(); i++) {
-		cout << sums[i] << ' ';
-	}
-	cout << endl;
-	*/
-
-	for (int i = n - 1; i >= 0; i--) {
-		for (int j = i; j >= 0; j--) {
-			int num = arr[i] - arr[j];
-
-			// cout << "arr[i] - arr[j] : " << num << '\n';
-			if (binarySearch(num, 0, sums.size() - 1) != -1) {
-				cout << arr[i];
+	for (int i = N - 1; i >= 0; i--) {
+		for (int j = N - 1; j >= 0; j--) {
+			bool ans = binary_search(sum.begin(), sum.end(), v[i] - v[j]);
+			if (ans) {
+				cout << v[i] << '\n';
 				return 0;
 			}
 		}
