56dc12230dadeb49a22b9f38fbc64088d802f4db
Jeon Wongyun <spotydol7@gmail.com>
2026-02-13 01:18:11 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 12 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -6,17 +6,19 @@ int dp[10001][4];
 int main() {
 	int T;
 	cin >> T;
+
+	dp[0][1] = 1;
+	dp[1][1] = 1;
+
+	for (int i = 2; i < 10001; i++) {
+		dp[i][1] = dp[i - 1][1];
+		dp[i][2] = dp[i - 1][1] + dp[i - 2][2];
+		if (i >= 3) dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+	}
+
 	while (T--) {
 		int N;
 		cin >> N;
-
-		dp[0][1] = 1;
-		dp[1][1] = 1;
-		for (int i = 2; i <= N; i++) {
-			dp[i][1] = dp[i - 1][1];
-			dp[i][2] = dp[i - 1][1] + dp[i - 2][2];
-			if (i >= 3) dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
-		}
 		cout << dp[N][1] + dp[N][2] + dp[N][3] << '\n';
 	}
 
