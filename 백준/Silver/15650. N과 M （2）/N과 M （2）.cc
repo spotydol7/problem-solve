3d885459ff6912c6fddb29be357e290c10a0ec84
Jeon Wongyun <spotydol7@gmail.com>
2026-02-17 19:18:54 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int arr[10];
+
+void dfs(int depth, int num) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = num; i <= N; i++) {
+		arr[depth] = i;
+		dfs(depth + 1, i + 1);
+	}
+}
+
+int main() {
+	cin >> N >> M;
+
+	dfs(0, 1);
+
+	return 0;
+}
