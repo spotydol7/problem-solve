6bb0ce68477af820ee4e7826c8608c2f44f858a9
Jeon Wongyun <spotydol7@gmail.com>
2026-02-17 20:27:30 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+vector<int> nums;
+int arr[10];
+bool visited[10];
+
+void dfs(int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	int last = INT_MAX;
+
+	for (int i = 0; i < N; i++) {
+		if (!visited[i] && last != nums[i]) {
+			visited[i] = true;
+			arr[depth] = nums[i];
+			last = nums[i];
+			dfs(depth + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		nums.push_back(temp);
+	}
+	sort(nums.begin(), nums.end());
+
+	dfs(0);
+	
+	return 0;
+}
